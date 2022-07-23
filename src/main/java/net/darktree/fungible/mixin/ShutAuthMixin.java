25e63dddf5e3b8dabfb51e0a5f0edb9fa4f23fde
jacg <jacg311@github.fake>
2022-07-23 23:29:03 +0200

    Fixes - revert back to 1 GB in gradle.properties - remove dev env check in ShutAuthMixin   - still keeping it an inject because other mods might inject there too. (for example iris does that) if you want to test with them. - re-add javadoc :D

--- src/main/java/net/darktree/fungible/mixin/ShutAuthMixin.java
+++ src/main/java/net/darktree/fungible/mixin/ShutAuthMixin.java
@@ -2,8 +2,6 @@ package net.darktree.fungible.mixin;
 
 import com.mojang.authlib.minecraft.UserApiService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
-import net.darktree.fungible.Fungible;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.RunArgs;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,11 +11,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class ShutAuthMixin {
+	/**
+	 * @author magistermaks
+	 * @reason STOP TAKING THE FOCUS AWAY FORM THE GAME LOG!
+	 */
 	@Inject(method = "createUserApiService", at = @At("HEAD"), cancellable = true)
 	private void fungible$supressAuthError(YggdrasilAuthenticationService authService, RunArgs runArgs, CallbackInfoReturnable<UserApiService> cir) {
-		if(FabricLoader.getInstance().isDevelopmentEnvironment()) {
-			Fungible.LOGGER.info("Suppressing Auth Error in dev env");
 			cir.setReturnValue(UserApiService.OFFLINE);
-		}
 	}
 }

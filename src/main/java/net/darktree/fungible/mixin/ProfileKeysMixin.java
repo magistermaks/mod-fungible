ca6e4b2413fbae77bb5470bfac19a8bc734939c1
jacg <jacg311@github.fake>
2022-07-23 23:46:15 +0200

    More Fixes:  Overwrite, not Inject - use @Overwrite instead of @Inject

--- src/main/java/net/darktree/fungible/mixin/ProfileKeysMixin.java
+++ src/main/java/net/darktree/fungible/mixin/ProfileKeysMixin.java
@@ -1,15 +1,12 @@
 package net.darktree.fungible.mixin;
 
 import com.mojang.authlib.minecraft.UserApiService;
-import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.util.ProfileKeys;
 import net.minecraft.network.encryption.PlayerKeyPair;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Optional;
 import java.util.concurrent.CompletableFuture;
@@ -22,11 +19,9 @@ public abstract class ProfileKeysMixin {
      * @author jacg311
      * @reason distracting message we dont care about in the dev env
      */
-    @Inject(method = "getKeyPair", at = @At("HEAD"), cancellable = true)
-    private void getKeyPair(UserApiService userApiService, CallbackInfoReturnable<CompletableFuture<Optional<PlayerKeyPair>>> cir) {
-        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
-            this.saveKeyPairToFile(null);
-            cir.setReturnValue(CompletableFuture.completedFuture(Optional.empty()));
-        }
+    @Overwrite
+    private CompletableFuture<Optional<PlayerKeyPair>> getKeyPair(UserApiService userApiService) {
+        this.saveKeyPairToFile(null);
+        return CompletableFuture.completedFuture(Optional.empty());
     }
 }

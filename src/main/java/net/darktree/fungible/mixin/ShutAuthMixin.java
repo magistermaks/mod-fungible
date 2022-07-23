594d85c3dfd9fcc4f49812258635e590dae43e16
jacg <jacg311@github.fake>
2022-07-23 23:48:05 +0200

    More Fixes:  Overwrite, not Inject - use @Overwrite instead of @Inject

--- src/main/java/net/darktree/fungible/mixin/ShutAuthMixin.java
+++ src/main/java/net/darktree/fungible/mixin/ShutAuthMixin.java
@@ -5,9 +5,7 @@ import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.RunArgs;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.Overwrite;
 
 @Mixin(MinecraftClient.class)
 public class ShutAuthMixin {
@@ -15,8 +13,8 @@ public class ShutAuthMixin {
 	 * @author magistermaks
 	 * @reason STOP TAKING THE FOCUS AWAY FORM THE GAME LOG!
 	 */
-	@Inject(method = "createUserApiService", at = @At("HEAD"), cancellable = true)
-	private void fungible$supressAuthError(YggdrasilAuthenticationService authService, RunArgs runArgs, CallbackInfoReturnable<UserApiService> cir) {
-			cir.setReturnValue(UserApiService.OFFLINE);
+	@Overwrite
+	private UserApiService createUserApiService(YggdrasilAuthenticationService authService, RunArgs runArgs) {
+		return UserApiService.OFFLINE;
 	}
 }

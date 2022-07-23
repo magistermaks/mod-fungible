9a58bb51ade3fdecc017b0a769eb144c6228c49f
jacg <jacg311@github.fake>
2022-07-23 22:36:21 +0200

    Update to 1.18 - Change mod version to include mc version - Update ShutAuthMixin to work in 1.18

--- src/main/java/net/darktree/fungible/mixin/ShutAuthMixin.java
+++ src/main/java/net/darktree/fungible/mixin/ShutAuthMixin.java
@@ -1,23 +1,23 @@
 package net.darktree.fungible.mixin;
 
-import com.mojang.authlib.minecraft.OfflineSocialInteractions;
-import com.mojang.authlib.minecraft.SocialInteractionsService;
+import com.mojang.authlib.minecraft.UserApiService;
 import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
+import net.darktree.fungible.Fungible;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.RunArgs;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class ShutAuthMixin {
-
-	/**
-	 * @author magistermaks
-	 * @reason STOP TAKING THE FOCUS AWAY FORM THE GAME LOG!
-	 */
-	@Overwrite
-	private SocialInteractionsService createSocialInteractionsService(YggdrasilAuthenticationService yggdrasilAuthenticationService, RunArgs runArgs) {
-		return new OfflineSocialInteractions();
+	@Inject(method = "createUserApiService", at = @At("HEAD"), cancellable = true)
+	private void fungible$supressAuthError(YggdrasilAuthenticationService authService, RunArgs runArgs, CallbackInfoReturnable<UserApiService> cir) {
+		if(FabricLoader.getInstance().isDevelopmentEnvironment()) {
+			Fungible.LOGGER.info("Suppressing Auth Error in dev env");
+			cir.setReturnValue(UserApiService.OFFLINE);
+		}
 	}
-
 }

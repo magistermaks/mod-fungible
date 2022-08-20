959a0a2565d7b0a2eb16db8b9cdc8a864641320f
magistermaks <magistermaks@darktree.net>
2022-08-20 12:46:39 +0200

    Made not crash on 1.19.2, disabled ProfileKeysMixin for now

--- src/main/java/net/darktree/fungible/mixin/ProfileKeysMixin.java
+++ src/main/java/net/darktree/fungible/mixin/ProfileKeysMixin.java
@@ -1,15 +1,10 @@
 package net.darktree.fungible.mixin;
 
-import com.mojang.authlib.minecraft.UserApiService;
 import net.minecraft.client.util.ProfileKeys;
 import net.minecraft.network.encryption.PlayerKeyPair;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-
-import java.util.Optional;
-import java.util.concurrent.CompletableFuture;
 
 @Mixin(ProfileKeys.class)
 public abstract class ProfileKeysMixin {
@@ -17,13 +12,14 @@ public abstract class ProfileKeysMixin {
     @Shadow
     protected abstract void saveKeyPairToFile(@Nullable PlayerKeyPair keyPair);
 
-    /**
-     * @author jacg311
-     * @reason distracting message we dont care about in the dev env
-     */
-    @Overwrite
-    private CompletableFuture<Optional<PlayerKeyPair>> getKeyPair(UserApiService userApiService) {
-        this.saveKeyPairToFile(null);
-        return CompletableFuture.completedFuture(Optional.empty());
-    }
+//    /**
+//     * @author jacg311
+//     * @reason distracting message we don't care about in the dev env
+//     */
+//    @Overwrite
+//    private CompletableFuture<Optional<PlayerKeyPair>> getKeyPair(UserApiService userApiService) {
+//        this.saveKeyPairToFile(null);
+//        return CompletableFuture.completedFuture(Optional.empty());
+//    }
+
 }

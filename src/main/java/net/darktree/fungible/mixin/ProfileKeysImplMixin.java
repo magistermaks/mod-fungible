4362f5ecaa12215222cd2c31463de2a05be12a86
jacg <jacg311@github.fake>
2023-01-04 19:34:06 +0100

    - Add ProfileKeysImplMixin back

---
+++ src/main/java/net/darktree/fungible/mixin/ProfileKeysImplMixin.java
@@ -0,0 +1,21 @@
+package net.darktree.fungible.mixin;
+
+import net.minecraft.client.util.ProfileKeysImpl;
+import net.minecraft.network.encryption.PlayerKeyPair;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+
+@Mixin(ProfileKeysImpl.class)
+public class ProfileKeysImplMixin {
+    /**
+     * @author jacg311
+     * @reason dont try to fetch the key
+     */
+    @Overwrite
+    public CompletableFuture<Optional<PlayerKeyPair>> fetchKeyPair() {
+        return CompletableFuture.completedFuture(Optional.empty());
+    }
+}
